d6192a40c9dc893a4ba49e82b2aa3b41b072c087
ryst4655 <ryst4655@colorado.edu>
2022-11-01 10:37:50 -0600

    ica11

---
+++ ICA/ica11/ica11.js
@@ -0,0 +1,30 @@
+// window.addEventListener('load', getQuote);
+
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', getQuote);
+
+const endPoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote(){
+    // console.log('test click worked');
+    let text = await fetch(endPoint);
+    let response = await text.text();
+    // .then(text => text.text());
+    let json_response = JSON.parse(response);
+    // console.log(json_response);
+    console.log(json_response['message']);
+
+    displayQuote(json_response['message']);
+
+}
+
+function displayQuote(x){
+    // const quoteBox = document.querySelector('#js-quote-text');
+    // console.log('display quote working');
+    // const textMessage = document.createTextNode(x);
+
+    // quoteBox.appendChild(textMessage);
+
+    document.getElementById('js-quote-text').textContent = x;
+}
+getQuote();
