d0f6a9cb37aae0a043d3fb9f5ffda02243eda3b1
ryst4655 <ryst4655@colorado.edu>
2022-11-08 00:07:07 -0700

    Revert "new"
    This reverts commit 8c0474e73a5c89683ead1581c99aef830b041417.

--- wa12/wa12.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-}
